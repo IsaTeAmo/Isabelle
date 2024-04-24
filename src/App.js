b74b26df27abdf449aa1630f94469371b27b35ac
Dorivis <dorival.jrr@gmail.com>
2024-04-24 00:10:25 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Isa te amo</h1>
+        <div className='heart'></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
